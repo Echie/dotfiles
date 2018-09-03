38a40e57b830ae7739c0c2315c72b212ba18b29e
Huovinen Teemu <teemu.huovinen@hotmail.fi>
2018-09-03 14:56:07 +0300

    Initial version of personalized setup

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 main() {
-    configure_plist_apps # Configure all apps whose configurations are plists
     configure_finder
     configure_dock
     configure_chrome
@@ -10,30 +9,29 @@ main() {
     move_focus_back_to_iterm2
 }
 
-function configure_plist_apps() {
-    #quit "Amphetamine"
-    quit "GIPHY CAPTURE"
-    quit "Transmission"
-    quit "The Unarchiver"
-    #import_plist "com.if.Amphetamine" "Amphetamine.plist"
-    import_plist "com.fasthatchapps.gifgrabberosx" "GIPHY_Capture.plist"
-    import_plist "org.m0k.transmission" "Transmission.plist"
-    import_plist "cx.c3.theunarchiver" "The_Unarchiver.plist"
-    #open "Amphetamine"
-}
-
-function configure_system() {
-    # Disable Gatekeeper entirely to get rid of \
-    # “Are you sure you want to open this application?” dialog
-    sudo spctl --master-disable
-}
-
-function configure_chrome() {
-    # Disable the all too sensitive backswipe on trackpads
-    defaults write com.google.Chrome \
-        AppleEnableSwipeNavigateWithScrolls -bool false
-    defaults write com.google.Chrome.canary \
-        AppleEnableSwipeNavigateWithScrolls -bool false
+function configure_finder() {
+    # Save screenshots to Downloads folder
+    defaults write com.apple.screencapture location -string "${HOME}/Downloads"
+    # Require password immediately after sleep or screen saver begins
+    defaults write com.apple.screensaver askForPassword -int 1
+    defaults write com.apple.screensaver askForPasswordDelay -int 0
+    # allow quitting via ⌘ + q; doing so will also hide desktop icons
+    defaults write com.apple.finder QuitMenuItem -bool true
+    # disable status bar
+    defaults write com.apple.finder ShowStatusBar -bool false
+    # disable path bar
+    defaults write com.apple.finder ShowPathbar -bool false
+    # Display full POSIX path as Finder window title
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    # Keep folders on top when sorting by name
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
+    # When performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    # Use list view in all Finder windows by default
+    # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+    # Disable the warning before emptying the Trash
+    defaults write com.apple.finder WarnOnEmptyTrash -bool false
 }
 
 function configure_dock() {
@@ -44,12 +42,6 @@ function configure_dock() {
     defaults write com.apple.dock persistent-apps -array
     # Show only open applications in the Dock
     defaults write com.apple.dock static-only -bool true
-    # Don’t animate opening applications from the Dock
-    defaults write com.apple.dock launchanim -bool false
-    # Disable Dashboard
-    defaults write com.apple.dashboard mcx-disabled -bool true
-    # Don’t show Dashboard as a Space
-    defaults write com.apple.dock dashboard-in-overlay -bool true
     # Automatically hide and show the Dock
     defaults write com.apple.dock autohide -bool true
     # Remove the auto-hiding Dock delay
@@ -69,9 +61,6 @@ function configure_dock() {
     ## 10: Put display to sleep
     ## 11: Launchpad
     ## 12: Notification Center
-    ## Top left screen corner → Mission Control
-    defaults write com.apple.dock wvous-tl-corner -int 2
-    defaults write com.apple.dock wvous-tl-modifier -int 0
     ## Top right screen corner → Desktop
     defaults write com.apple.dock wvous-tr-corner -int 0
     defaults write com.apple.dock wvous-tr-modifier -int 0
@@ -81,42 +70,18 @@ function configure_dock() {
     open "Dock"
 }
 
-function configure_finder() {
-    # Save screenshots to Downloads folder
-    defaults write com.apple.screencapture location -string "${HOME}/Downloads"
-    # Require password immediately after sleep or screen saver begins
-    defaults write com.apple.screensaver askForPassword -int 1
-    defaults write com.apple.screensaver askForPasswordDelay -int 0
-    # allow quitting via ⌘ + q; doing so will also hide desktop icons
-    defaults write com.apple.finder QuitMenuItem -bool true
-    # disable window animations and Get Info animations
-    defaults write com.apple.finder DisableAllAnimations -bool true
-    # Set Downloads as the default location for new Finder windows
-    defaults write com.apple.finder NewWindowTarget -string "PfLo"
-    defaults write com.apple.finder NewWindowTargetPath -string \
-        "file://${HOME}/Downloads/"
-    # disable status bar
-    defaults write com.apple.finder ShowStatusBar -bool false
-    # disable path bar
-    defaults write com.apple.finder ShowPathbar -bool false
-    # Display full POSIX path as Finder window title
-    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-    # Keep folders on top when sorting by name
-    defaults write com.apple.finder _FXSortFoldersFirst -bool true
-    # When performing a search, search the current folder by default
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-    # Disable disk image verification
-    defaults write com.apple.frameworks.diskimages \
-        skip-verify -bool true
-    defaults write com.apple.frameworks.diskimages \
-        skip-verify-locked -bool true
-    defaults write com.apple.frameworks.diskimages \
-        skip-verify-remote -bool true
-    # Use list view in all Finder windows by default
-    # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-    # Disable the warning before emptying the Trash
-    defaults write com.apple.finder WarnOnEmptyTrash -bool false
+function configure_chrome() {
+    # Disable the all too sensitive backswipe on trackpads
+    defaults write com.google.Chrome \
+        AppleEnableSwipeNavigateWithScrolls -bool false
+    defaults write com.google.Chrome.canary \
+        AppleEnableSwipeNavigateWithScrolls -bool false
+}
+
+function configure_system() {
+    # Disable Gatekeeper entirely to get rid of \
+    # “Are you sure you want to open this application?” dialog
+    sudo spctl --master-disable
 }
 
 function quit() {
@@ -124,25 +89,18 @@ function quit() {
     killall "$app" > /dev/null 2>&1
 }
 
-function open() {
-    app=$1
-    osascript << EOM
-tell application "$app" to activate
-EOM
-}
-
-function import_plist() {
-    domain=$1
-    filename=$2
-    defaults delete "$domain" &> /dev/null
-    defaults import "$domain" "$filename"
-}
-
 function move_focus_back_to_iterm2() {
     osascript << EOM
 tell application "System Events" to tell process "iTerm2"
 set frontmost to true
 end tell
+EOM
+}
+
+function open() {
+    app=$1
+    osascript << EOM
+tell application "$app" to activate
 EOM
 }
 
--- setup-mac-os.sh
+++ setup-mac-os.sh
@@ -16,35 +16,30 @@ main() {
     clone_dotfiles_repo
     # Installing all packages in Dotfiles repository's Brewfile
     install_packages_with_brewfile
-    # Changing default shell to Fish
-    change_shell_to_fish
-    # Configuring git config file
+    # Install oh-my-zsh
+    install_oh_my_zsh
+    # Make sure zsh is the default shell
+    change_shell_to_zsh
+    # Configuring git config files
     configure_git
-    # Installing powerline-status so that setup_symlinks can setup the symlinks
-    # and requests and dotenv as the basis for a regular python script
-    pip_packages=(powerline-status requests python-dotenv flake8)
+    # Install scm breeze
+    configure_scm_breeze
+    # Configure spectacle
+    configure_spectacle
+    # Install global python packages
+    pip_packages=(virtualenv pylint yapf)
     pip3_install "${pip_packages[@]}"
-    # Installing typescript so that YouCompleteMe can support it
-    # and prettier so that Neoformat can auto-format files
-    yarn_packages=(prettier typescript)
-    yarn_install "${yarn_packages[@]}"
-    # Setting up symlinks so that setup_vim can install all plugins
-    setup_symlinks
-    # Setting up Vim
-    setup_vim
-    # Setting up tmux
-    setup_tmux
     # Configuring iTerm2
     configure_iterm2
-    # Update /etc/hosts
-    update_hosts_file
+    # Update /etc/hosts (Currently disabled)
+    # update_hosts_file
     # Setting up macOS defaults
     setup_macOS_defaults
     # Updating login items
     update_login_items
 }
 
-DOTFILES_REPO=~/personal/dotfiles
+DOTFILES_REPO=~/.dotfiles
 
 function ask_for_sudo() {
     info "Prompting for sudo password..."
@@ -59,42 +54,18 @@ function ask_for_sudo() {
     fi
 }
 
-function login_to_app_store() {
-    info "Logging into app store..."
-    if mas account >/dev/null; then
-        success "Already logged in."
-    else
-        open -a "/Applications/App Store.app"
-        until (mas account > /dev/null);
-        do
-            sleep 3
-        done
-        success "Login to app store successful."
-    fi
-}
-
 function install_homebrew() {
     info "Installing Homebrew..."
     if hash brew 2>/dev/null; then
         success "Homebrew already exists."
     else
-url=https://raw.githubusercontent.com/Sajjadhosn/dotfiles/master/installers/homebrew_installer
+        url=https://raw.githubusercontent.com/echie/dotfiles/master/installers/homebrew_installer
         if /usr/bin/ruby -e "$(curl -fsSL ${url})"; then
             success "Homebrew installation succeeded."
         else
             error "Homebrew installation failed."
             exit 1
         fi
-    fi
-}
-
-function install_packages_with_brewfile() {
-    info "Installing packages within ${DOTFILES_REPO}/brew/macOS.Brewfile ..."
-    if brew bundle --file=$DOTFILES_REPO/brew/macOS.Brewfile; then
-        success "Brewfile installation succeeded."
-    else
-        error "Brewfile installation failed."
-        exit 1
     fi
 }
 
@@ -113,44 +84,17 @@ function brew_install() {
     fi
 }
 
-function change_shell_to_fish() {
-    info "Fish shell setup..."
-    if grep --quiet fish <<< "$SHELL"; then
-        success "Fish shell already exists."
+function login_to_app_store() {
+    info "Logging into app store..."
+    if mas account >/dev/null; then
+        success "Already logged in."
     else
-        user=$(whoami)
-        substep "Adding Fish executable to /etc/shells"
-        if grep --fixed-strings --line-regexp --quiet \
-            "/usr/local/bin/fish" /etc/shells; then
-            substep "Fish executable already exists in /etc/shells"
-        else
-            if echo /usr/local/bin/fish | sudo tee -a /etc/shells > /dev/null;
-            then
-                substep "Fish executable successfully added to /etc/shells"
-            else
-                error "Failed to add Fish executable to /etc/shells"
-                exit 1
-            fi
-        fi
-        substep "Switching shell to Fish for \"${user}\""
-        if sudo chsh -s /usr/local/bin/fish "$user"; then
-            success "Fish shell successfully set for \"${user}\""
-        else
-            error "Please try setting the Fish shell again."
-        fi
-    fi
-}
-
-function configure_git() {
-    username="Sajjad Hosseini"
-    email="sajjad.hosseini@futurice.com"
-
-    info "Configuring git..."
-    if git config --global user.name "$username" && \
-       git config --global user.email "$email"; then
-        success "git configuration succeeded."
-    else
-        error "git configuration failed."
+        open -a "/Applications/App Store.app"
+        until (mas account > /dev/null);
+        do
+            sleep 3
+        done
+        success "Login to app store successful."
     fi
 }
 
@@ -160,7 +104,7 @@ function clone_dotfiles_repo() {
         substep "${DOTFILES_REPO} already exists."
         pull_latest $DOTFILES_REPO
     else
-        url=https://github.com/Sajjadhosn/dotfiles.git
+        url=https://github.com/echie/dotfiles.git
         if git clone "$url" $DOTFILES_REPO; then
             success "Cloned into ${DOTFILES_REPO}"
         else
@@ -170,68 +114,109 @@ function clone_dotfiles_repo() {
     fi
 }
 
-function pull_latest() {
-    info "Pulling latest changes in ${1} repository..."
-    if git -C $1 pull origin master &> /dev/null; then
-        success "Pull successful in ${1} repository."
+function install_packages_with_brewfile() {
+    info "Installing packages within ${DOTFILES_REPO}/brew/macOS.Brewfile ..."
+    if brew bundle --file=$DOTFILES_REPO/brew/macOS.Brewfile; then
+        success "Brewfile installation succeeded."
     else
-        error "Please pull the latest changes in ${1} repository manually."
+        error "Brewfile installation failed."
+        exit 1
     fi
 }
 
-function setup_vim() {
-    info "Setting up vim..."
-    substep "Installing Vundle"
-    if test -e ~/.vim/bundle/Vundle.vim; then
-        substep "Vundle already exists."
-        pull_latest ~/.vim/bundle/Vundle.vim
+function install_oh_my_zsh() {
+    info "Installing Oh My Zsh..."
+    if hash brew 2>/dev/null; then
+        success "Homebrew already exists."
     else
-        url=https://github.com/VundleVim/Vundle.vim.git
-        if git clone "$url" ~/.vim/bundle/Vundle.vim; then
-            substep "Vundle installation succeeded."
+        url=https://raw.githubusercontent.com/echie/dotfiles/master/installers/oh_my_zsh_installer
+        if sh -c "$(curl -fsSL ${url})"; then
+            success "Oh My Zsh installation succeeded."
         else
-            error "Vundle installation failed."
+            error "Oh My Zsh installation failed."
             exit 1
         fi
     fi
-    substep "Installing all plugins"
-    if vim +PluginInstall +qall 2> /dev/null; then
-        substep "Plugin installation succeeded."
-    else
-        error "Plugin installation failed."
-        exit 1
-    fi
-    success "vim successfully setup."
 }
 
-function setup_tmux() {
-    info "Setting up tmux..."
-    substep "Installing tpm"
-    if test -e ~/.tmux/plugins/tpm; then
-        substep "tpm already exists."
-        pull_latest ~/.tmux/plugins/tpm
+function change_shell_to_zsh() {
+    info "Zsh shell setup..."
+    if grep --quiet zsh <<< "$SHELL"; then
+        success "Zsh shell already exists."
     else
-        url=https://github.com/tmux-plugins/tpm
-        if git clone "$url" ~/.tmux/plugins/tpm; then
-            substep "tpm installation succeeded."
+        user=$(whoami)
+        substep "Adding zsh executable to /etc/shells"
+        if grep --fixed-strings --line-regexp --quiet \
+            "/usr/local/bin/zsh" /etc/shells; then
+            substep "Zsh executable already exists in /etc/shells"
         else
-            error "tpm installation failed."
-            exit 1
+            if echo /usr/local/bin/zsh | sudo tee -a /etc/shells > /dev/null;
+            then
+                substep "Zsh executable successfully added to /etc/shells"
+            else
+                error "Failed to add Zsh executable to /etc/shells"
+                exit 1
+            fi
+        fi
+        substep "Switching shell to Zsh for \"${user}\""
+        if sudo chsh -s /usr/local/bin/zsh "$user"; then
+            success "Zsh shell successfully set for \"${user}\""
+        else
+            error "Please try setting the Zsh shell again."
         fi
     fi
+}
 
-    substep "Installing all plugins"
+function configure_git() {
+    username="Teemu Huovinen"
+    email="teemu.huovinen@hotmail.fi"
 
-    # sourcing .tmux.conf is necessary for tpm
-    tmux source-file ~/.tmux.conf 2> /dev/null
-
-    if ~/.tmux/plugins/tpm/bin/./install_plugins; then
-        substep "Plugin installation succeeded."
+    info "Configuring git..."
+    if cp ${DOTFILES_REPO}/git/.gitignore_global ~/.gitignore_global && \
+    cp ${DOTFILES_REPO}/git/gitconfig ~/gitconfig; then
+        success "Git configuration succeeded."
     else
-        error "Plugin installation failed."
-        exit 1
+        error "Git configuration failed."
     fi
-    success "tmux successfully setup."
+}
+
+function configure_scm_breeze() {
+    info "Configuring scm_breeze..."
+    if git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze && \
+       ~/.scm_breeze/install.sh && \
+       source ~/.zshrc; then
+        success "Scm breeze configuration succeeded."
+    else
+        error "Scm breeze configuration failed."
+    fi
+}
+
+function configure_spectacle() {
+    info "Configuring up spectacle..."
+    if cp ${DOTFILES_REPO}/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json; then
+        success "Spectacle successfully configured."
+    else
+        error "Spectacle configuration failed"
+    fi
+}
+
+function pip3_install() {
+    packages_to_install=("$@")
+
+    for package_to_install in "${packages_to_install[@]}"
+    do
+        info "pip3 install ${package_to_install}"
+        if pip3 --quiet show "$package_to_install"; then
+            success "${package_to_install} already exists."
+        else
+            if pip3 install "$package_to_install"; then
+                success "Package ${package_to_install} installation succeeded."
+            else
+                error "Package ${package_to_install} installation failed."
+                exit 1
+            fi
+        fi
+    done
 }
 
 function configure_iterm2() {
@@ -252,49 +237,6 @@ function configure_iterm2() {
         substep "iTerm2 activation successful"
     else
         error "Failed to activate iTerm2"
-        exit 1
-    fi
-}
-
-function setup_symlinks() {
-    POWERLINE_ROOT_REPO=/usr/local/lib/python2.7/site-packages
-
-    info "Setting up symlinks..."
-    symlink "vim" ${DOTFILES_REPO}/vim/vimrc ~/.vimrc
-    symlink "tmux" ${DOTFILES_REPO}/tmux/tmux.conf ~/.tmux.conf
-    symlink "powerline" \
-        ${DOTFILES_REPO}/powerline \
-        ${POWERLINE_ROOT_REPO}/powerline/config_files
-    #symlink "spectacle" \
-    # the above line should be commented out and used instead of the "cp" below
-    # when Spectacle fixes the sorting issue of Shortcuts.json file
-    cp \
-        ${DOTFILES_REPO}/spectacle/Shortcuts.json \
-        ~/Library/Application\ Support/Spectacle/Shortcuts.json
-    symlink "fish:completions" ${DOTFILES_REPO}/fish/completions \
-        ~/.config/fish/completions
-    symlink "fish:functions" ${DOTFILES_REPO}/fish/functions \
-        ~/.config/fish/functions
-    symlink "fish:config.fish" ${DOTFILES_REPO}/fish/config.fish \
-        ~/.config/fish/config.fish
-    symlink "fish:oh_my_fish" ${DOTFILES_REPO}/fish/oh_my_fish  ~/.config/omf
-    success "Symlinks successfully setup."
-}
-
-function symlink() {
-    application=$1
-    point_to=$2
-    destination=$3
-    destination_dir=$(dirname "$destination")
-
-    if test ! -e "$destination_dir"; then
-        substep "Creating ${destination_dir}"
-        mkdir -p "$destination_dir"
-    fi
-    if rm -rf "$destination" && ln -s "$point_to" "$destination"; then
-        success "Symlinking ${application} done."
-    else
-        error "Symlinking ${application} failed."
         exit 1
     fi
 }
@@ -341,16 +283,8 @@ function setup_macOS_defaults() {
 
 function update_login_items() {
     info "Updating login items..."
-    login_item /Applications/Alfred\ 3.app
-    login_item /Applications/Amphetamine.app
-    login_item /Applications/Bartender\ 3.app
-    login_item /Applications/Docker.app
     login_item /Applications/Dropbox.app
-    login_item /Applications/iTerm.app
-    login_item /Applications/HighSierraMediaKeyEnabler.app
     login_item /Applications/Spectacle.app
-    login_item /Applications/NordVPN.app
-    login_item /Applications/1Password\ 6.app
     success "Login items successfully updated."
 }
 
@@ -372,42 +306,13 @@ else
 fi
 }
 
-function yarn_install() {
-    packages_to_install=("$@")
-
-    for package_to_install in "${packages_to_install[@]}"
-    do
-        info "yarn global add ${package_to_install}"
-        if yarn global list | grep "$package_to_install" &> /dev/null; then
-            success "${package_to_install} already exists."
-        else
-            if yarn global add "$package_to_install"; then
-                success "Package ${package_to_install} installation succeeded."
-            else
-                error "Package ${package_to_install} installation failed."
-                exit 1
-            fi
-        fi
-    done
-}
-
-function pip3_install() {
-    packages_to_install=("$@")
-
-    for package_to_install in "${packages_to_install[@]}"
-    do
-        info "pip3 install ${package_to_install}"
-        if pip3 --quiet show "$package_to_install"; then
-            success "${package_to_install} already exists."
-        else
-            if pip3 install "$package_to_install"; then
-                success "Package ${package_to_install} installation succeeded."
-            else
-                error "Package ${package_to_install} installation failed."
-                exit 1
-            fi
-        fi
-    done
+function pull_latest() {
+    info "Pulling latest changes in ${1} repository..."
+    if git -C $1 pull origin master &> /dev/null; then
+        success "Pull successful in ${1} repository."
+    else
+        error "Please pull the latest changes in ${1} repository manually."
+    fi
 }
 
 function coloredEcho() {
